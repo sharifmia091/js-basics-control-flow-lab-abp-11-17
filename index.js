d6d591e170318d3dfcdf50dee24963cd90c916b5
md sharif mia <sharifmia091@gmail.com>
2017-11-17 12:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(string){
   if (string <=400){
     return "This one is on me!";
   }
-  else if (string>=2000) {
+  else if (string>=2000 && string<=2500) {
     return "I will gladly take your thirty bucks."
   }
   else {
