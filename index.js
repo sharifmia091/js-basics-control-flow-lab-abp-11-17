bc9e3860bbfcd505596e4424d66beb2f0b5483bc
md sharif mia <sharifmia091@gmail.com>
2017-11-17 13:24:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(string){
+  if (string <=400){
+    return "This one is on me!";
+  }
+  else if (string>=2000 && string<=2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do."
+  }
+}
+
+
+function ternaryCheckCity(string){
+  if("NYC"==string){
+    return  "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(string){
+  if ("generous"== string){
+    return "Thank you so much."
+  } else if ("not as generous" == string) {
+    return "Thank you."
+  } else {
+    return "Bye.";
+  }
+}
