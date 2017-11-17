32b37a03728ed1b8b18b1059f76f97ca5c2f4a64
md sharif mia <sharifmia091@gmail.com>
2017-11-17 13:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function ternaryCheckCity(string){
 }
 
 function switchOnCharmFromTip(string){
-  if ("generous"= string){
+  if (generous= string){
     return "Thank you so much."
   } else if (generous !== string) {
     return "Thank you."
